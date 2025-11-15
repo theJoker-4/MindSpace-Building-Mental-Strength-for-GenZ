0861fceb3f87907d629d20dcea210c86501ef461
MuscleBunny618 <prakharbaranwal618@gmail.com>
2025-11-15 22:34:46 +0530

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -32,12 +32,12 @@ export const TEAM_MEMBERS: TeamMember[] = [
   {
     name: 'Prakhar Baranwal',
     role: 'Project Lead, UI Designer and Project Tester',
-    avatarUrl: 'https://github.com/theJoker-4/MindSpace-Building-Mental-Strength-for-GenZ/blob/main/images/IMG_20251112_011932.jpg?raw=true'
+    avatarUrl: 'https://github.com/theJoker-4/MindSpace-Building-Mental-Strength-for-GenZ/main/images/IMG_20251112_011932.jpg?raw=true'
   },
   {
     name: 'Himank Choudhary',
     role: 'Frontend and Backend Developer, UI Designer',
-    avatarUrl: 'https://github.com/theJoker-4/MindSpace-Building-Mental-Strength-for-GenZ/blob/main/images/1%20(1).jpg?raw=true'
+    avatarUrl: 'https://github.com/theJoker-4/MindSpace-Building-Mental-Strength-for-GenZ/main/images/1%20(1).jpg?raw=true'
   },
   {
     name: 'Divyansh Batra',
